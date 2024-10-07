5ed0df90e7ac4729121625e493f6a3ac871e4aa8
Appsupportinfotech <appsupport@shobhitinfotech.com>
2024-10-07 17:32:54 +0530

    luckygame

---
+++ frst.js
@@ -0,0 +1,9 @@
+//practice 2nd
+let luckynum = 11;
+let harshitnum = prompt("guess the number :");
+
+while (harshitnum != luckynum) {
+    harshitnum = prompt("oops you entered wrong number . guess again :"); 
+}
+
+console.log("congratulations dear, you entered the right number you win 100000");
